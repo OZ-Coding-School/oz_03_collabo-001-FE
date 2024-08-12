f14ca96a0cc16ceb495960c62b6c0d9a3c3b30ed
소빈 <sobin11@naver.com>
2024-08-12 21:54:07 +0900

    ✨ Feat: App.tsx 라우팅설정 및 페이지연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/layout/Header';
+import FooterGnb from './components/layout/FooterGnb';
+import Home from './pages/Home/Home';
+
 function App() {
-  return <div className='bg-primary'>home</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <FooterGnb />
+      </Router>
+    </>
+  );
 }
 
 export default App;
