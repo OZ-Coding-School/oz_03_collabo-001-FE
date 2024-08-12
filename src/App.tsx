ca5bed93f8e30cb0a65f5dbd0043acc3945a768f
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-12 22:28:35 +0900

    ✨ Feat: 추가 App.tsx에 로그인 컴포넌트 임포트
    Fixes: #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Login from './page/Login/Login';
+
 function App() {
-  return <div className='bg-primary'>home</div>;
+  return (
+    <div className='h-screen bg-[#ffffff]'>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
