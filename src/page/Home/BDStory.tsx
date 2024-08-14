ea12006464e5ec58c4486af6ea72b36a78f3a677
Supernova-428 <89819295+Supernova-428@users.noreply.github.com>
2024-08-14 14:51:19 +0900

    ✨ Feat: 장소 컴포넌트 추가 외 7 (#19)
    * ✨ Feat: 장소 컴포넌트 추가
    
    Fixes: #1
    
    * ✨ Feat: 플레이스 지역별 / 장소별 tab
    
    Fixes: #7
    
    * ✨ Feat: 메인 홈 배너
    
    Fixes: #11
    
    * ✨ Feat: 리뷰 작성 페이지 상단 유저정보 컴포넌트
    
    Fixes: #14
    
    * ✨ Feat: 메인 홈 애개 매거진 컴포넌트
    
    Fixes: #15
    
    * ✨ Feat: 메인 홈 애개 스토리 컴포넌트
    
    Fixes: #17
    
    * ✨ Feat: 뒤로가기 헤더 컴포넌트
    
    Fixes: #18
    
    * 💄 Style: 추가 예시 이미지

---
+++ src/page/Home/BDStory.tsx
@@ -0,0 +1,26 @@
+import BgImage from '../../assets/images/starfield.png';
+
+const BDStory = () => {
+  return (
+    <div
+      className='flex h-[180px] w-[180px] flex-col-reverse overflow-clip rounded-lg border-2 border-border'
+      style={{
+        backgroundImage: `url(${BgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* <img src={BgImage} alt='' className='h-full w-full object-cover' /> */}
+      <div className='flex flex-col justify-between truncate text-nowrap bg-[#00000050] p-2 text-[white]'>
+        <p className='truncate text-[0.9rem] font-bold'>
+          {'가보니 너무 좋아 추천해요!'}
+        </p>
+        <p className='truncate text-nowrap text-[0.8rem] text-tag'>
+          {'오늘 저희 아가들이랑 댕댕이랑 가보니 너무 좋았어요'}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BDStory;
