893ed31f5ffdbc18bf9ba3c6b6dc77e5a40ab18c
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-14 14:55:50 +0900

    🚑 Fix: 수정 앰.tsx 충돌 방지 처음 상태로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import Login from './page/Login/Login';
-
 function App() {
-  return (
-    <div className='h-screen bg-[#ffffff]'>
-      <Login />
-    </div>
-  );
+  return <div className='h-screen bg-[#ffffff]'></div>;
 }
 
 export default App;
