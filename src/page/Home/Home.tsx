38dd67cfb9fdd2f60f5488ba8ee5e75e5412317d
소빈 <sobin11@naver.com>
2024-08-16 16:34:50 +0900

    ✨ Feat: 여러 컴포넌트를 통합하여 페이지 구성 완료
    Fixes: #23

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Banner from './Banner';
+import MiddleNav from './MiddleNav';
+import RecoPlace from '../../components/BDPlace/RecoPlace';
 
 const Home = () => {
-  return <div className='h-[1200px] bg-primary'>home</div>;
+  return (
+    <div className='h-[1200px] bg-background'>
+      <Banner />
+      <MiddleNav />
+      <RecoPlace />
+    </div>
+  );
 };
 
 export default Home;
