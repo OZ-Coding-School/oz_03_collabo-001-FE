c879d6d641f158824896982624768c312b428b69
소빈 <sobin11@naver.com>
2024-08-16 18:20:00 +0900

    ✨ Feat: 추가 - footer에 링크 연결
    Fixes: #2

--- src/components/layout/FooterGnb.tsx
+++ src/components/layout/FooterGnb.tsx
@@ -8,13 +8,14 @@ import iconMag from '../../assets/Icon/Nav_Bottom/BK_Magazine.svg';
 import iconMy from '../../assets/Icon/Nav_Bottom/BK_My.svg';
 
 interface LinkProps {
+  link: string;
   img: string;
   text: string;
 }
 
-const FooterLink: React.FC<LinkProps> = ({ img, text }) => {
+const FooterLink: React.FC<LinkProps> = ({ link, img, text }) => {
   return (
-    <Link to='/' className='flex w-1/5 items-center'>
+    <Link to={link} className='flex w-1/5 items-center'>
       <div className='w-full text-center'>
         <img
           src={img}
@@ -30,12 +31,12 @@ const FooterLink: React.FC<LinkProps> = ({ img, text }) => {
 
 const FooterGnb = () => {
   return (
-    <footer className='bg-white fixed bottom-0 left-1/2 flex h-[46px] w-[400px] -translate-x-1/2'>
-      <FooterLink img={iconHome} text='홈' />
-      <FooterLink img={iconPlace} text='애개플레이스' />
-      <FooterLink img={iconMain} text='애개육아정보' />
-      <FooterLink img={iconMag} text='애개매거진' />
-      <FooterLink img={iconMy} text='마이' />
+    <footer className='fixed bottom-0 left-1/2 flex h-[46px] w-[400px] -translate-x-1/2 bg-white'>
+      <FooterLink link='/' img={iconHome} text='홈' />
+      <FooterLink link='/' img={iconPlace} text='애개플레이스' />
+      <FooterLink link='/' img={iconMain} text='애개육아정보' />
+      <FooterLink link='/' img={iconMag} text='애개매거진' />
+      <FooterLink link='/mypage' img={iconMy} text='마이' />
     </footer>
   );
 };
