cb9f1e4cf485fac0cb259ebb4dc2f5f1518ef72b
Supernova-428 <89819295+Supernova-428@users.noreply.github.com>
2024-08-19 15:01:08 +0900

    ✨ Feat: 추가 애개플레이스, 펫존, 키즈존 홈  (#30)
    * 🎨 Design: 수정 크기 및 텍스트속성
    
    Fixes #1
    
    * ✨ Feat: 수정 필터 Props
    필터링할 탭 내용을 placeHome에서 받아오기 위해서 props추가
    Fixes #7
    
    * ✨ Feat: 수정 PlaceHome 컴포넌트 연결
    
    fixes: #5 Related to: #24
    
    * ✨ Feat: 추가 애개플레이스, 펫존, 키즈존 홈
    
    Fixes: #24

--- src/components/BDPlace/Place.tsx
+++ src/components/BDPlace/Place.tsx
@@ -11,7 +11,7 @@ const Place = () => {
   };
 
   return (
-    <div className='relative h-[22vh] w-[22vh] overflow-clip rounded-lg border-2 border-border bg-[white]'>
+    <div className='relative h-[120px] w-[120px] overflow-clip rounded-lg border-[1px] border-border bg-[white]'>
       <img src={BgImage} alt='' className='h-3/5 w-full object-cover' />
       <label className='absolute right-1 top-1'>
         <input type='checkbox' onClick={handleBoockMark} className='hidden' />
@@ -21,9 +21,9 @@ const Place = () => {
           className='h-[3vh]'
         />
       </label>
-      <div className='flex h-2/5 flex-col justify-between p-2'>
-        <p className='text-[0.9rem] font-bold'>{`[${'경기'}] ${'스타필드 고양'}`}</p>
-        <p className='flex gap-1 text-[0.8rem]'>
+      <div className='flex h-2/5 flex-col justify-between p-1.5'>
+        <p className='truncate text-nowrap text-[12px] font-semibold'>{`[${'경기'}] ${'스타필드 고양'}`}</p>
+        <p className='flex gap-1 text-[10px]'>
           <span className='text-primary'>★</span>
           <span>{'4.8'}</span>
           <span className='text-caption'>{`(${'3'})`}</span>
---
+++ src/page/PlaceHome/PlaceHome.tsx
@@ -0,0 +1,97 @@
+import Place from '../../components/BDPlace/Place';
+import RegionTab from '../../components/BDPlace/RegionTab';
+import Banner from '../Home/Banner';
+
+interface CurrentProps {
+  current: string;
+}
+
+const BDPlaceHome: React.FC<CurrentProps> = ({ current }) => {
+  return (
+    <>
+      <Banner />
+      <div className='flex flex-col gap-[15px] pb-[46px]'>
+        <div className='bg-white p-[10px]'>
+          <p>추천장소</p>
+        </div>
+        <div className='flex flex-col gap-[10px] bg-white p-[10px]'>
+          <p>
+            {current === 'BD'
+              ? '새로생긴 애개플레이스'
+              : current === 'pet'
+                ? '새로생긴 펫존'
+                : '새로생긴 키즈존'}
+          </p>
+          <div className='grid grid-cols-3 grid-rows-2 gap-[8px]'>
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+          </div>
+        </div>
+        <div className='flex flex-col gap-[10px] bg-white p-[10px]'>
+          <div className='flex items-center justify-between'>
+            <p>
+              {current === 'BD'
+                ? '지역별 애개플레이스'
+                : current === 'pet'
+                  ? '지역별 펫존'
+                  : '지역별 키즈존'}
+            </p>
+            <button className='font-medium'>〉</button>
+          </div>
+          <div>
+            <RegionTab
+              tabs={[
+                '전체',
+                '서울',
+                '경기',
+                '인천',
+                '충청',
+                '강원',
+                '전라',
+                '경상',
+                '제주',
+              ]}
+            />
+          </div>
+          <div className='grid grid-cols-3 grid-rows-2 gap-[8px]'>
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+          </div>
+        </div>
+        <div className='flex flex-col gap-[10px] bg-white p-[10px]'>
+          <div className='flex items-center justify-between'>
+            <p>
+              {current === 'BD'
+                ? '장소별 애개플레이스'
+                : current === 'pet'
+                  ? '장소별 펫존'
+                  : '장소별 키즈존'}
+            </p>
+            <button>〉</button>
+          </div>
+          <div>
+            <RegionTab tabs={['전체', '카페', '펜션', '음식점', '야외/공원']} />
+          </div>
+          <div className='grid grid-cols-3 grid-rows-2 gap-[8px]'>
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+            <Place />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BDPlaceHome;
