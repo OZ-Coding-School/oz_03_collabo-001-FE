fa45e769e06b6594f8c48c0f8fda21318f2389a2
소빈 <sobin11@naver.com>
2024-08-19 20:54:02 +0900

    ✨ Feat: 추가 - 애개육아정보 인기글 컴포넌트
    Related to : #32

---
+++ src/page/Home/BDInfoPopular.tsx
@@ -0,0 +1,79 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import MoreTitle from '../../components/layout/MoreTitle';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+import BDInfoPopularItem from './BDInfoPopularItem';
+
+export default function BDInfoPopular() {
+  const swiperSlides = Array(4).fill(null); // 4개의 SwiperSlide 생성
+  const items = Array(2).fill(null); // 2개의 BDInfoPopularItem 생성
+
+  return (
+    <div className='card card2'>
+      <MoreTitle title='애개육아정보 인기글' />
+      <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        speed={500}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className='recoSwiper m-0 w-full'
+      >
+        {/* <SwiperSlide>
+          <BDInfoPopularItem />
+          <BDInfoPopularItem />
+          <BDInfoPopularItem className='last' />
+        </SwiperSlide> */}
+
+        {swiperSlides.map((_, index) => (
+          <SwiperSlide key={index}>
+            {items.map((_, idx) => (
+              <BDInfoPopularItem key={idx} />
+            ))}
+            <BDInfoPopularItem className='last' />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <style>{`
+        .recoSwiper .swiper-pagination-bullet {
+            background: #dddddd;
+            width: 8px;
+            height: 8px;
+            border-radius: 1rem;
+            opacity: 1;
+            transition-duration: 500ms;
+            transition-timing-function: ease-out;
+        }
+        .recoSwiper .swiper-pagination-bullet-active {
+            background: #f78222;
+            border-radius: 1rem;
+            opacity: 1;
+            transition-duration: 500ms;
+            transition-timing-function: ease-out;
+        }
+        .recoSwiper .swiper-button-prev,
+        .recoSwiper .swiper-button-next {
+            display:none;
+        }
+        .recoSwiper .swiper-pagination {
+            margin-top: 16px;
+            margin-bottom: 8px;
+            display:flex;
+            justify-content: center;
+            position: static;
+        }
+    `}</style>
+    </div>
+  );
+}
---
+++ src/page/Home/BDInfoPopularItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface BDInfoPopularItemProps {
+  className?: string;
+}
+
+const BDInfoPopularItem: React.FC<BDInfoPopularItemProps> = ({ className }) => {
+  const borderClass = classNames({
+    'border-b border-border': className !== 'last',
+    'flex items-center py-[15px]': true,
+  });
+
+  return (
+    <div className={borderClass}>
+      <span className='h-[16px] w-[16px] rounded-10px bg-primary text-center text-[10px] leading-[16px] text-white'>
+        1
+      </span>
+      <div className='imgWrap ml-[8px] h-[47px] w-[47px] rounded-10px bg-background'></div>
+      <div className='ml-[8px]'>
+        <p className='text-[14px] font-semibold'>강아지털 이대로 괜찮을까요?</p>
+        <p className='text-[12px]'>너무 고민하지마세요.</p>
+        <div className='text-[12px]'>
+          <span>글쓴이</span>
+          <span className='ml-[10px] text-caption'>2024.01.01</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BDInfoPopularItem;
