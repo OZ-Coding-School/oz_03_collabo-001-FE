220de3d75e47990ba8d44175994cfb96bc2d4912
소빈 <sobin11@naver.com>
2024-08-19 13:58:39 +0900

    ✨ Feat: FooterLink 컴포넌트에서 현재 경로와 일치하는 경우 활성화 클래스 추가, 아이콘 변경
    Fixes: #2

--- src/components/layout/FooterGnb.tsx
+++ src/components/layout/FooterGnb.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
-import iconHome from '../../assets/Icon/Nav_Bottom/BK_Home.svg';
-import iconPlace from '../../assets/Icon/Nav_Bottom/BK_BDplace.svg';
-import iconMain from '../../assets/Icon/Nav_Bottom/BK_Main.svg';
-import iconMag from '../../assets/Icon/Nav_Bottom/BK_Magazine.svg';
-import iconMy from '../../assets/Icon/Nav_Bottom/BK_My.svg';
+import BKiconHome from '../../assets/Icon/Nav_Bottom/BK_Home.svg';
+import BKiconPlace from '../../assets/Icon/Nav_Bottom/BK_BDplace.svg';
+import BKiconMain from '../../assets/Icon/Nav_Bottom/BK_Main.svg';
+import BKiconMag from '../../assets/Icon/Nav_Bottom/BK_Magazine.svg';
+import BKiconMy from '../../assets/Icon/Nav_Bottom/BK_My.svg';
+import ORiconHome from '../../assets/Icon/Nav_Bottom/OR_Home.svg';
+import ORiconPlace from '../../assets/Icon/Nav_Bottom/OR_BDplace.svg';
+import ORiconMain from '../../assets/Icon/Nav_Bottom/OR_Main.svg';
+import ORiconMag from '../../assets/Icon/Nav_Bottom/OR_Magazine.svg';
+import ORiconMy from '../../assets/Icon/Nav_Bottom/OR_My.svg';
 
 interface LinkProps {
   link: string;
-  img: string;
+  BKimg: string;
+  ORimg: string;
   text: string;
 }
 
-const FooterLink: React.FC<LinkProps> = ({ link, img, text }) => {
+const FooterLink: React.FC<LinkProps> = ({ link, BKimg, ORimg, text }) => {
+  const location = useLocation();
+  const isActive = location.pathname === link;
+
   return (
-    <Link to={link} className='flex w-1/5 items-center'>
+    <Link
+      to={link}
+      className={twMerge(
+        'flex w-1/5 items-center',
+        isActive ? '!font-semibold text-primary' : ''
+      )}
+    >
       <div className='w-full text-center'>
         <img
-          src={img}
+          src={isActive ? ORimg : BKimg}
           alt=''
           aria-hidden
           className='mx-auto w-[18px] pt-[3px]'
@@ -32,11 +48,31 @@ const FooterLink: React.FC<LinkProps> = ({ link, img, text }) => {
 const FooterGnb = () => {
   return (
     <footer className='fixed bottom-0 left-1/2 flex h-[46px] w-[400px] -translate-x-1/2 bg-white'>
-      <FooterLink link='/' img={iconHome} text='홈' />
-      <FooterLink link='/' img={iconPlace} text='애개플레이스' />
-      <FooterLink link='/' img={iconMain} text='애개육아정보' />
-      <FooterLink link='/' img={iconMag} text='애개매거진' />
-      <FooterLink link='/mypage' img={iconMy} text='마이' />
+      <FooterLink link='/' BKimg={BKiconHome} ORimg={ORiconHome} text='홈' />
+      <FooterLink
+        link='/bdPlaceHome'
+        BKimg={BKiconPlace}
+        ORimg={ORiconPlace}
+        text='애개플레이스'
+      />
+      <FooterLink
+        link='/bdInfo'
+        BKimg={BKiconMain}
+        ORimg={ORiconMain}
+        text='애개육아정보'
+      />
+      <FooterLink
+        link='/bdMag'
+        BKimg={BKiconMag}
+        ORimg={ORiconMag}
+        text='애개매거진'
+      />
+      <FooterLink
+        link='/mypage'
+        BKimg={BKiconMy}
+        ORimg={ORiconMy}
+        text='마이페이지'
+      />
     </footer>
   );
 };
