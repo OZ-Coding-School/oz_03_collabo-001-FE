22e0d46d4ceed69358ee0b7a5505b35adf7bbe74
소빈 <sobin11@naver.com>
2024-08-20 14:33:28 +0900

    ✨ Feat: 추가 - 내가 작성한 글 텍스트 커스텀훅 사용하여 말줄임처리
    Fixes: #36

--- src/page/MyPage/WritingList.tsx
+++ src/page/MyPage/WritingList.tsx
@@ -5,12 +5,19 @@ const WritingList = () => {
   const reviewItemCount = 2;
   const reviewItems = Array.from({ length: reviewItemCount });
 
+  const boardText =
+    '친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요 친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께많아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께많아요';
+
   return (
     <>
       <div className='last card card2'>
         <MoreTitle title='내가 작성한 글' />
         {reviewItems.map((_, index) => (
-          <WritingListItem key={index} className={index === 0 ? 'first' : ''} />
+          <WritingListItem
+            key={index}
+            className={index === 0 ? 'first' : ''}
+            boardText={boardText}
+          />
         ))}
       </div>
     </>
