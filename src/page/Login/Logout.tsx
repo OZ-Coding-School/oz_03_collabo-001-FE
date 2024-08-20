f77821390541d7c420ef8c5a939a27a3b7da5d61
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-20 10:44:44 +0900

    ✨ Feat: 추가 로그아웃 기능 구글 로그아웃 기능 구현 Fixes: #4

---
+++ src/page/Login/Logout.tsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    axios
+      .post(
+        'http://localhost:8000/users/google/logout/',
+        {},
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log('로그아웃 성공:', response);
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.error('로그아웃 실패:', error);
+      });
+  };
+
+  return (
+    <button className='bg-white' onClick={handleLogout}>
+      <p className='text-[10px] font-normal text-caption underline'>로그아웃</p>
+    </button>
+  );
+};
+
+export default Logout;
