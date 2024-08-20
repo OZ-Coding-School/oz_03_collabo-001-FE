58c1590d4ffa831a199a73825164f5cb6d45aa8f
2_현주 <164320612+Yi-HyeonJu@users.noreply.github.com>
2024-08-20 15:46:51 +0900

    💡 Comment: 변경 토큰 검증 컴포넌트 주석으로 외 16 (#37)
    * ✨ Feat: 추가 토스트 패키지 설치
    
    Fixes: #4
    
    * 💄 Style: 삭제 구글 로그인 파일 주석
    
    Fixes: #4
    
    * 🚚 Rename: 변경 파일명
    리다이렉트 -> 어스
    Fixes: #4
    
    * 💄 Style: 변경 함수명
    함수 이름에 핸들 추가
    Fixes: #4
    
    * ✨ Feat: 추가 로그아웃 기능
    구글 로그아웃 기능 구현
    Fixes: #4
    
    * ✨ Feat: 추가 토큰 검증 컴포넌트
    최상단에 위치하는 검증 컴포넌트
    FixesL #33
    
    * ✨ Feat: 추가 토큰 검증 함수
    엑세스 토큰 검증 및 리프레쉬로 재발급
    Fixes: #33
    
    * ✨ Feat: 추가 컴포넌트 임포트
    - 로그인 페이지, 토스트, 검증 컴포넌트들 임포트
    - 푸터 없는 주소에 로그인페이지들 추가
    Fixes: #2 #4
    
    * ✨ Feat: 추가 로그아웃 컴포넌트 임포트
    임의로 임포트
    
    * 🔥 Remove: 삭제 개별로 인가코드 받는 페이지
    인가코드 받는 주소 통합을 위해 삭제
    Fixes: #4
    
    * ✨ Feat: 추가 인가코드 서버에 전송 기능
    각 소셜 로그인별로 서버와 소통
    Fixes: #4
    
    * 🔥 Remove: 삭제 인가코드 찍는 콘솔 로그
    인가 코드 받아오는지 테스트 끝
    Fixes: #4
    
    * ✨ Feat: 추가 상태 파라미터
    사용자가 클릭한 소셜 구별을 위해 파라미터 추가
    Fixes: #4
    
    * 🚑 Fix: 변경 로그아웃 엔드포인트
    
    Fixes: #4
    
    * ✨ Feat: 변경 인가코드 받는 주소
    통합한 주소로 변경
    Fixes: #4
    
    * 🚑 Fix: 수정 검증, 재발급 엔트포인트
    
    Fixes: #4
    
    * 💡 Comment: 변경 토큰 검증 컴포넌트 주석으로
    임시 설정이라 주석처리 하였음
    Fixes: #2

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,35 @@ import FooterGnb from './components/layout/FooterGnb';
 import Home from './page/Home/Home';
 import MyPage from './page/MyPage/MyPage';
 import PlaceFilter from './page/PlaceFilter/PlaceFilter';
+import Login from './page/Login/Login';
+// import PrivateRoute from './components/PrivateRoute';
+import AuthProvider from './page/Login/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const location = useLocation();
-  const noHeaderPaths: string[] = ['/placefilter', '/mypage']; // Header를 숨길 경로들
+
+  const noHeaderPaths: string[] = ['/placefilter', '/mypage', '/login', '/auth/provider']; // Header를 숨길 경로들
+  const noFooterPaths: string[] = ['/login', '/auth/provider']; // Footer를 숨길 경로들
 
   return (
     <>
+      <ToastContainer />
+
       {!noHeaderPaths.includes(location.pathname) && <Header />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/mypage' element={<MyPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/auth/provider' element={<AuthProvider />} />\
+        {/* <Route
+          path='/placefilter'
+          element={<PrivateRoute element={PlaceFilter} />}
+        /> */}
         <Route path='/placefilter' element={<PlaceFilter />} />
       </Routes>
-      <FooterGnb />
+      {!noFooterPaths.includes(location.pathname) && <FooterGnb />}
     </>
   );
 }
---
+++ src/hooks/useVerify.tsx
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// 액세스 토큰을 서버에서 검증하는 API 호출
+export const Api = {
+  // 토큰 검증 API 호출
+  userTokenVerify: async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8000/users/token/verify/',
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error('토큰 검증 오류 : ', error);
+      throw error;
+    }
+  },
+
+  // 토큰 갱신 API 호출
+  userTokenRefresh: async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8000/users/token/refresh/',
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error('토큰 갱신 오류 :', error);
+      throw error;
+    }
+  },
+};
+
+const useVerify = () => {
+  const nav = useNavigate();
+
+  const checkLoginStatus = useCallback(async () => {
+    try {
+      await Api.userTokenVerify();
+    } catch (error) {
+      console.log('토큰 검증 실패. 재시도..');
+      try {
+        await Api.userTokenRefresh();
+        await Api.userTokenVerify();
+      } catch (error) {
+        console.error('토큰 검증 및 갱신 실패 :', error);
+        nav('/login', { replace: true }); // 검증 및 갱신 실패 시 로그인 페이지로 리디렉션
+      }
+    }
+  }, [nav]);
+
+  return {
+    checkLoginStatus,
+  };
+};
+
+export default useVerify;
