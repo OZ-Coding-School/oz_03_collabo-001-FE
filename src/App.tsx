c87db96d3e5a6b538b31b4fa3555bf6995010028
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-20 10:48:41 +0900

    ✨ Feat: 추가 컴포넌트 임포트 - 로그인 페이지, 토스트, 검증 컴포넌트들 임포트 - 푸터 없는 주소에 로그인페이지들 추가 Fixes: #2 #4

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,34 @@ import Header from './components/layout/Header';
 import FooterGnb from './components/layout/FooterGnb';
 import Home from './page/Home/Home';
 import PlaceFilter from './page/PlaceFilter/PlaceFilter';
+import Login from './page/Login/Login';
+import GoogleAuth from './page/Login/Google/GoogleAuth';
+import PrivateRoute from './components/PrivateRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const location = useLocation();
-  const noHeaderPaths: string[] = ['/placefilter']; // Header를 숨길 경로들
+
+  const noHeaderPaths: string[] = ['/placefilter', '/login', '/google/auth']; // Header를 숨길 경로들
+  const noFooterPaths: string[] = ['/login', '/google/auth']; // Footer를 숨길 경로들
 
   return (
     <>
+      <ToastContainer />
+
       {!noHeaderPaths.includes(location.pathname) && <Header />}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/placefilter' element={<PlaceFilter />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/google/auth' element={<GoogleAuth />} />
+        <Route
+          path='/placefilter'
+          element={<PrivateRoute element={PlaceFilter} />}
+        />
+        {/* <Route path='/placefilter' element={<PlaceFilter />} /> */}
       </Routes>
-      <FooterGnb />
+      {!noFooterPaths.includes(location.pathname) && <FooterGnb />}
     </>
   );
 }
