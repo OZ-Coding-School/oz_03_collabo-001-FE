93b6656a68ad41c2f737a0a16a1ad7a8165992cc
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-20 10:51:07 +0900

    ✨ Feat: 추가 로그아웃 컴포넌트 임포트 임의로 임포트

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import Logout from '../Login/Logout';
 
 const Home = () => {
-  return <div className='h-[1200px] bg-primary'>home</div>;
+  return (
+    <div className='h-[1200px] bg-primary'>
+      home
+      <Logout />
+    </div>
+  );
 };
 
 export default Home;
