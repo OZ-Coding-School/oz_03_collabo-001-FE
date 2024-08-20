057244c8ca851e1ecc63c5d6673bb387740ce846
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-20 14:33:50 +0900

    ✨ Feat: 추가 상태 파라미터 사용자가 클릭한 소셜 구별을 위해 파라미터 추가 Fixes: #4

--- src/page/Login/Google/GoogleLogin.tsx
+++ src/page/Login/Google/GoogleLogin.tsx
@@ -3,7 +3,8 @@ const GoogleLogin = () => {
 		response_type=code
 		&scope=email
 		&client_id=${import.meta.env.VITE_REACT_APP_GOOGLE_AUTH_CLIENT_ID}
-		&redirect_uri=${import.meta.env.VITE_REACT_APP_GOOGLE_AUTH_REDIRECT_URI}`;
+		&redirect_uri=${import.meta.env.VITE_REACT_APP_REDIRECT_URI}
+		&state=google`;
 };
 
 export default GoogleLogin;
--- src/page/Login/Naver/NaverLogin.tsx
+++ src/page/Login/Naver/NaverLogin.tsx
@@ -1,10 +1,9 @@
 const NaverLogin = () => {
-  // 구글 로그인 화면으로 이동시키기
   window.location.href = `https://nid.naver.com/oauth2.0/authorize?
 		response_type=code
-		&state=babydog
 		&client_id=${import.meta.env.VITE_REACT_APP_NAVER_CLIENT_ID}
-		&redirect_uri=${import.meta.env.VITE_REACT_APP_NAVER_REDIRECT_URI}`;
+		&redirect_uri=${import.meta.env.VITE_REACT_APP_REDIRECT_URI}
+		&state=naver`;
 };
 
 export default NaverLogin;
