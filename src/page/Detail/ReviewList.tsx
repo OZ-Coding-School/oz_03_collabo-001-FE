9106ad40a64f1f19cae8205a2407f2845fdce208
소빈 <sobin11@naver.com>
2024-08-21 14:57:22 +0900

    ✨ Feat: 추가 - 디테일페이지 후기 컴포넌트
    Related to: #45

---
+++ src/page/Detail/ReviewList.tsx
@@ -0,0 +1,50 @@
+import ReviewListItem from './ReviewListItem';
+
+const ReviewList = () => {
+  // 후기의 갯수
+  const reviewCount = 50;
+  // 후기 내용
+  const reviewText =
+    '친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요 친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요 아이들이랑 강아지랑 놀께 많아요친절하고 좋아요';
+  //
+  const reviewItemCount = 3;
+  const reviewItems = Array.from({ length: reviewItemCount });
+
+  return (
+    <div>
+      <div className='cardTitle flex items-center justify-between'>
+        <p className='font-semibold'>
+          후기 <span className='text-primary'>{reviewCount}</span>개
+        </p>
+      </div>
+      {reviewCount <= 0 ? (
+        <NoReview />
+      ) : (
+        <>
+          {reviewItems.map((_, index) => {
+            return (
+              <ReviewListItem
+                key={index}
+                className={index === 2 ? 'noBorder' : ''}
+                reviewText={reviewText}
+              />
+            );
+          })}
+          <button className='mb-[30px] h-[35px] w-full rounded-[5px] border-2 border-border text-center text-[14px]'>
+            후기 {reviewCount}개 모두보기
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+const NoReview = () => {
+  return (
+    <div className='p-[30px] text-center text-[14px] text-caption'>
+      작성된 후기가 없습니다.
+    </div>
+  );
+};
+
+export default ReviewList;
