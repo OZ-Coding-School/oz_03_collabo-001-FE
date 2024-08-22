28bfcf332864aae700f7c95e71ad6233a90a591b
dos_no <tlatms777@gmail.com>
2024-08-22 17:27:22 +0900

    ♻️ Refactor: 변경 마이페이지 userinfo 기능별로 나눔
    Fixes: #9

--- src/components/Mypage/UserInfo.tsx
+++ src/components/Mypage/UserInfo.tsx
@@ -1,39 +1,27 @@
-import defaultProfile from '../../assets/DefaultProfile.svg';
 import Logout from '../../page/Login/Logout';
+import { Link } from 'react-router-dom';
+import ProfilePhotoEdit from './ProfileEdit';
+import UserNameEditor from './NicknameEdit';
 
 const UserInfo: React.FC = () => {
   return (
     <div className='flex justify-center bg-white py-[20px]'>
       <div className='flex h-[119px] items-center rounded-[10px] border-[0.5px] border-[#b3b3b3] px-[15px] py-[20px]'>
-        <div className='mr-[18px] flex h-[78px] w-[79px] flex-col items-center justify-center'>
-          <img
-            className='mb-[5px] h-[66px] w-[66px]'
-            src={defaultProfile}
-            alt='프로필 기본이미지'
-          ></img>
-          <p className='text-[10px] text-caption underline'>프로필변경</p>
-        </div>
+        <ProfilePhotoEdit />
         <div>
-          <div className='flex justify-center'>
-            <div className='h-[24px] w-[201px] text-[18px] font-bold'>
-              작성자이름(별명)
-            </div>
-            <p className='flex h-[24px] w-[45px] items-center justify-center text-[10px] text-caption underline'>
-              이름변경
-            </p>
-          </div>
+          <UserNameEditor />
           <div className='mt-[5px] text-[12px] text-caption'>
             <p>12345@naver.com(계정정보)</p>
-            <Logout />
+            {/* <Logout /> */}
           </div>
         </div>
       </div>
-      {/* // //비로그인 상태
-        <div className='flex h-[119px] w-[360px] items-center rounded-[10px] border-[0.5px] border-[#b3b3b3] px-[15px] py-[20px]'>
-          <Link to='/login'>
-            <button className='text-[18px] font-bold'>로그인하기</button>
-          </Link>
-        </div> */}
+      {/* //비로그인 상태 */}
+      {/* <div className='flex h-[119px] w-[360px] items-center rounded-[10px] border-[0.5px] border-[#b3b3b3] px-[15px] py-[20px]'>
+        <Link to='/login'>
+          <button className='text-[18px] font-bold'>로그인하기</button>
+        </Link>
+      </div> */}
     </div>
   );
 };
