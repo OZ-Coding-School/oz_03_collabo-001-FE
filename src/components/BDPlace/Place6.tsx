31f7adda6d735245607b2051d6af42905146963f
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-23 17:16:55 +0900

    ♻️ Refactor: 수정 컴포넌트 분리 컴포넌트 분리 Fixes: #58 Related to: #1

---
+++ src/components/BDPlace/Place6.tsx
@@ -0,0 +1,16 @@
+import Place from './Place';
+
+const Place6 = () => {
+  return (
+    <div className='grid grid-cols-3 grid-rows-2 gap-[10px] py-2'>
+      <Place />
+      <Place />
+      <Place />
+      <Place />
+      <Place />
+      <Place />
+    </div>
+  );
+};
+
+export default Place6;
