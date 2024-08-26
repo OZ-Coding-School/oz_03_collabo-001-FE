7835a22c07bb6404f4cfe778aeb952e1696f7fd9
소빈 <sobin11@naver.com>
2024-08-26 16:25:39 +0900

    ✨ Feat: 추가 - 컴포넌트 별 모달 상태관리
    Fixes: #34, #35, #1

--- src/components/BDPlace/Place.tsx
+++ src/components/BDPlace/Place.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import useModalWithURL from '../../hooks/useModalWithURL';
 import BgBookMarkLine from '../../assets/Icon/BookMark/Bg_BookMark_Line.svg';
 import BgBookMarkFill from '../../assets/Icon/BookMark/Bg_BookMark_Fill.svg';
 import BgImage from '../../assets/images/starfield.png';
+import DetailModal from '../modal/DetailModal';
 
-const Place = () => {
+interface PlaceProps {
+  placeId: string; // 각 Place에 고유한 ID를 부여하여 모달 상태를 구분
+}
+
+const Place: React.FC<PlaceProps> = ({ placeId }) => {
+  const { isOpen, openModal, closeModal } = useModalWithURL(
+    `detailModal_${placeId}`
+  );
+
   const [isBookMarked, setIsBookMarked] = useState<boolean>(false);
 
   const handleBoockMark = () => {
@@ -12,27 +21,34 @@ const Place = () => {
   };
 
   return (
-    <div className='relative h-[120px] w-[120px] overflow-clip rounded-lg border-[1px] border-border bg-[white]'>
-      <Link to='/detail/123'>
-        <img src={BgImage} alt='' className='h-3/5 w-full object-cover' />
-      </Link>
-      <label className='absolute right-1 top-1'>
-        <input type='checkbox' onClick={handleBoockMark} className='hidden' />
-        <img
-          src={isBookMarked ? BgBookMarkFill : BgBookMarkLine}
-          alt={isBookMarked ? BgBookMarkFill : BgBookMarkLine}
-          className='h-5'
-        />
-      </label>
-      <div className='flex h-2/5 flex-col justify-between p-1.5'>
-        <p className='truncate text-nowrap text-[12px] font-semibold'>{`[${'경기'}] ${'스타필드 고양'}`}</p>
-        <p className='flex gap-1 text-[10px]'>
-          <span className='text-primary'>★</span>
-          <span>{'4.8'}</span>
-          <span className='text-caption'>{`(${'3'})`}</span>
-        </p>
+    <>
+      <div className='relative h-[120px] w-[120px] overflow-clip rounded-lg border-[1px] border-border bg-[white]'>
+        <button
+          onClick={() => {
+            openModal();
+          }}
+        >
+          <img src={BgImage} alt='' className='h-3/5 w-full object-cover' />
+        </button>
+        <label className='absolute right-1 top-1'>
+          <input type='checkbox' onClick={handleBoockMark} className='hidden' />
+          <img
+            src={isBookMarked ? BgBookMarkFill : BgBookMarkLine}
+            alt={isBookMarked ? BgBookMarkFill : BgBookMarkLine}
+            className='h-5'
+          />
+        </label>
+        <div className='flex h-2/5 flex-col justify-between p-1.5'>
+          <p className='truncate text-nowrap text-[12px] font-semibold'>{`[${'경기'}] ${'스타필드 고양'}`}</p>
+          <p className='flex gap-1 text-[10px]'>
+            <span className='text-primary'>★</span>
+            <span>{'4.8'}</span>
+            <span className='text-caption'>{`(${'3'})`}</span>
+          </p>
+        </div>
       </div>
-    </div>
+      {isOpen && <DetailModal closeModal={closeModal} />}
+    </>
   );
 };
 
--- src/page/MyPage/RecentPlace.tsx
+++ src/page/MyPage/RecentPlace.tsx
@@ -1,15 +1,22 @@
+import useModalWithURL from '../../hooks/useModalWithURL';
 import MoreTitle from '../../components/layout/MoreTitle';
 import Place from '../../components/BDPlace/Place';
+import PlaceFilterModal from '../../components/modal/PlaceFilterModal';
 
 const RecentPlace = () => {
+  const { isOpen, openModal, closeModal } = useModalWithURL('recentModal');
+
   return (
     <div className='col'>
-      <MoreTitle title='최근 본 장소' />
+      <MoreTitle title='최근 본 장소' openModal={openModal} />
       <div className='gap-4px flex justify-between pb-[30px]'>
-        <Place />
-        <Place />
-        <Place />
+        <Place placeId='4' />
+        <Place placeId='5' />
+        <Place placeId='6' />
       </div>
+      {isOpen && (
+        <PlaceFilterModal title='최근 본 장소' closeModal={closeModal} />
+      )}
     </div>
   );
 };
