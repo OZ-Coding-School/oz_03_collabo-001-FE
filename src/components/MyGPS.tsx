d97cb14f60b2439e01c72ff688369f50de60364c
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-28 13:33:39 +0900

    ♻️ Refactor: 수정 코드를 좀 더 짧게 로직 간결하게 수정 및 로딩 텍스트 추가 Fixes: #76

--- src/components/MyGPS.tsx
+++ src/components/MyGPS.tsx
@@ -3,30 +3,26 @@ import useGeolocation from '../hooks/useGeolocation';
 import GPS from '../assets/GPS.svg';
 
 const MyGPS: React.FC = () => {
-  const { address, error, getLocation } = useGeolocation();
-
-  const getButtonText = () => {
-    if (error) return `오류 : ${error}`;
-    if (address) return `현재 위치 : ${address}`;
-    return '내 위치';
-  };
+  const { address, error, isLoading, getLocation } = useGeolocation();
 
   return (
-    <div>
-      <button
-        className='flex items-center'
-        onClick={() => {
-          if (!address) {
-            getLocation();
-          }
-        }}
-      >
-        <img src={GPS} alt='GPS icon' className='h-[10px] pr-1' />
-        <span className='text-[8px] text-caption hover:underline'>
-          {getButtonText()}
-        </span>
-      </button>
-    </div>
+    <button
+      className='flex items-center'
+      onClick={() => {
+        getLocation();
+      }}
+    >
+      <img src={GPS} alt='GPS icon' className='h-[10px] pr-1' />
+      <span className='text-[8px] text-caption hover:underline'>
+        {isLoading
+          ? '현위치를 가져오는 중입니다...'
+          : error
+            ? `오류 : ${error}`
+            : address
+              ? `현위치 : ${address}`
+              : '내 위치'}
+      </span>
+    </button>
   );
 };
 
