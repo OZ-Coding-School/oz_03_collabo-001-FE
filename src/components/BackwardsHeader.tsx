31de1b9429568f08b6dd313b0b6f88d06902d171
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-29 16:54:43 +0900

    ♻️ Refactor: 수정 복잡한 로직을 통해 뒤로가기 모달에 갇혀서 나가ㅣㅈ 못하는 오루 수정 Fixes: #93

--- src/components/BackwardsHeader.tsx
+++ src/components/BackwardsHeader.tsx
@@ -1,24 +1,39 @@
-// import { Outlet, useNavigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { GoChevronLeft } from 'react-icons/go';
 
 const BackwardsHeader = ({ title }: { title?: string }) => {
   const navigate = useNavigate();
+  const importantPaths = ['/', '/bdplacehome', '/bdinfo', '/bdmag'];
+
+  const handleBackClick = () => {
+    const referrerUrl = document.referrer;
+
+    if (referrerUrl) {
+      const url = new URL(referrerUrl);
+      const cleanedUrl = `${url.origin}${url.pathname}`;
+
+      // 도메인 및 경로 확인
+      if (url.origin === 'https://dogandbaby.co.kr') {
+        if (importantPaths.includes(url.pathname)) {
+          navigate(url.pathname);
+        } else {
+          navigate(cleanedUrl);
+        }
+      } else {
+        navigate('/');
+      }
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
-    <>
-      <div className='BackwardsHeader fixed top-0 z-50 flex h-[48px] w-[400px] items-center bg-[white] p-4 shadow'>
-        <button
-          onClick={() => navigate(-1)}
-          className='mr-[8px] font-extrabold'
-        >
-          <GoChevronLeft className='text-[24px] opacity-[70%]' />
-        </button>
-        <p>{title}</p>
-      </div>
-      {/* <div className='mt-[48px]'>
-        <Outlet />
-      </div> */}
-    </>
+    <div className='BackwardsHeader fixed top-0 z-50 flex h-[48px] w-[400px] items-center bg-[white] p-4 shadow'>
+      <button onClick={handleBackClick} className='mr-[8px] font-extrabold'>
+        <GoChevronLeft className='text-[24px] opacity-[70%]' />
+      </button>
+      <p>{title}</p>
+    </div>
   );
 };
 
