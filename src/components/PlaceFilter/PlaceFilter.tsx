9118940903c7967f5f3a4aee76374c732a3b10a3
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-29 17:53:00 +0900

    ♻️ Refactor: 수정 모달 이름 하드코딩 모달 이름이 하드코딩 되어있어서 프롭스로 받아 사용 Fixes: #58

--- src/components/PlaceFilter/PlaceFilter.tsx
+++ src/components/PlaceFilter/PlaceFilter.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-// import PlaceItem from '../../components/BDPlace/PlaceItem';
 import { GoChevronLeft } from 'react-icons/go';
 import FilterOptions from './FilterOptions';
 import PlaceList from './PlaceList';
@@ -8,9 +7,13 @@ import FilterDistance from './FilterDistance';
 
 interface PlaceHomeFilterProps {
   closeModal: () => void;
+  title: string;
 }
 
-const PlaceHomeFilter: React.FC<PlaceHomeFilterProps> = ({ closeModal }) => {
+const PlaceHomeFilter: React.FC<PlaceHomeFilterProps> = ({
+  closeModal,
+  title,
+}) => {
   useEffect(() => {
     document.body.style.overflow = 'hidden';
 
@@ -19,6 +22,15 @@ const PlaceHomeFilter: React.FC<PlaceHomeFilterProps> = ({ closeModal }) => {
     };
   }, []);
 
+  // 마지막 띄어쓰기 이후의 부분만 추출
+  const extractLastPart = (text: string): string => {
+    const lastIndex = text.lastIndexOf(' ');
+    if (lastIndex !== -1) {
+      return text.substring(lastIndex + 1);
+    }
+    return text; // 띄어쓰기가 없는 경우 원본 텍스트를 반환
+  };
+
   return ReactDOM.createPortal(
     <div className='h-100vh fixed inset-0 z-50 flex items-start justify-center bg-background'>
       <div className='flex h-[100%] flex-col overflow-y-scroll bg-white'>
@@ -26,7 +38,7 @@ const PlaceHomeFilter: React.FC<PlaceHomeFilterProps> = ({ closeModal }) => {
           <button onClick={closeModal} className='mr-[8px] font-extrabold'>
             <GoChevronLeft className='text-[24px] opacity-[70%]' />
           </button>
-          <p className='py-[18px] font-semibold'>애개플레이스</p>
+          <p className='py-[18px] font-semibold'>{extractLastPart(title)}</p>
         </div>
         <div className='flex items-center gap-[10px] px-3 py-[15px]'>
           <FilterOptions />
