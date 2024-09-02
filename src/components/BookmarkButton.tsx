342a1c4b516356bde9e463c71af52bb56a70f61c
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-02 17:08:23 +0900

    ✨ Feat: 추가 기존 북마크 상태 체크 후 서버에 전송
    Fixes: #104

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -8,15 +8,19 @@ import useAuthStore from '../store/authStore';
 
 interface BookmarkButtonProps {
   placeId: string;
+  isBookmarkedInitially: boolean; // 초기 북마크 상태를 받기 위한 prop
 }
 
-const BookmarkButton: React.FC<BookmarkButtonProps> = ({ placeId }) => {
-  const [bookMark, setBookMark] = useState(false);
+const BookmarkButton: React.FC<BookmarkButtonProps> = ({
+  placeId,
+  isBookmarkedInitially,
+}) => {
+  const [isBookmarked, setIsBookmarked] = useState(isBookmarkedInitially);
   const { checkLoginStatus } = useVerify();
   const { isAuthenticated } = useAuthStore();
 
   const handleBookmarkToggle = async () => {
-    if (!isAuthenticated)
+    if (!isAuthenticated) {
       return toast.error('로그인 후 사용해주세요!', {
         position: 'top-center',
         autoClose: 5000,
@@ -27,20 +31,28 @@ const BookmarkButton: React.FC<BookmarkButtonProps> = ({ placeId }) => {
         progress: undefined,
         theme: 'light',
       });
+    }
 
     try {
       await checkLoginStatus();
-      setBookMark(true);
-
-      await axios.post(
-        `http://127.0.0.1:8000/places/${placeId}/bookmark/`,
-        {},
-        { withCredentials: true }
-      );
+      if (isBookmarked) {
+        // 북마크 제거 요청
+        await axios.delete(
+          `http://127.0.0.1:8000/places/${placeId}/bookmark/`,
+          { withCredentials: true }
+        );
+        setIsBookmarked(false);
+      } else {
+        // 북마크 추가 요청
+        await axios.post(
+          `http://127.0.0.1:8000/places/${placeId}/bookmark/`,
+          {},
+          { withCredentials: true }
+        );
+        setIsBookmarked(true);
+      }
     } catch (error) {
-      setBookMark(false);
-      console.error('북마크 저장 실패:', error);
-
+      console.error('북마크 처리 실패:', error);
       toast.error('로그인 상태를 확인해주세요!', {
         position: 'top-center',
         autoClose: 5000,
@@ -61,7 +73,7 @@ const BookmarkButton: React.FC<BookmarkButtonProps> = ({ placeId }) => {
       onClick={handleBookmarkToggle}
     >
       <img
-        src={bookMark ? BookmarkFill : BookmarkLine}
+        src={isBookmarked ? BookmarkFill : BookmarkLine}
         alt='북마크 아이콘'
         className='h-5'
         aria-hidden
