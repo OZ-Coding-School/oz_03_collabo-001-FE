9b6e1e133f7e29e4f89ef8b31ad49da72299d34d
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-02 15:45:29 +0900

    ✨ Feat: 추가 로그인 상태 확인 후 서버에 북마크 상태 저장
    Fixes: #104

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookmarkLine from '../assets/Icon/BookMark/Bg_BookMark_Line.svg';
 import BookmarkFill from '../assets/Icon/BookMark/Bg_BookMark_Fill.svg';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import useVerify from '../hooks/useVerify';
+import useAuthStore from '../store/authStore';
 
 interface BookmarkButtonProps {
   placeId: string;
-  isBookmarked: boolean; // 프롭스로 isBookmarked를 추가
-  onToggle: () => void;
 }
 
-const BookmarkButton: React.FC<BookmarkButtonProps> = ({
-  isBookmarked,
-  onToggle,
-}) => {
+const BookmarkButton: React.FC<BookmarkButtonProps> = ({ placeId }) => {
+  const [bookMark, setBookMark] = useState(false);
+  const { checkLoginStatus } = useVerify();
+  const { isAuthenticated } = useAuthStore();
+
+  const handleBookmarkToggle = async () => {
+    if (!isAuthenticated)
+      return toast.error('로그인 후 사용해주세요!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+
+    try {
+      await checkLoginStatus();
+      setBookMark(true);
+
+      await axios.post(
+        `http://127.0.0.1:8000/places/${placeId}/bookmark/`,
+        {},
+        { withCredentials: true }
+      );
+    } catch (error) {
+      setBookMark(false);
+      console.error('북마크 저장 실패:', error);
+
+      toast.error('로그인 상태를 확인해주세요!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+  };
+
   return (
-    <button type='button' aria-label='북마크하기' onClick={onToggle}>
+    <button
+      type='button'
+      aria-label='북마크하기'
+      onClick={handleBookmarkToggle}
+    >
       <img
-        src={isBookmarked ? BookmarkFill : BookmarkLine}
+        src={bookMark ? BookmarkFill : BookmarkLine}
         alt='북마크 아이콘'
         className='h-5'
         aria-hidden
