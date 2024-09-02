2ba7802ca1e53bd53645c0952258da2a3b4b3f5f
소빈 <sobin11@naver.com>
2024-09-02 23:23:18 +0900

    ✨ Feat: 추가 - detail 상세내용 이미지 컨테이너의 총 길이 계산을 위해 ref변경 및 내용없음 처리 추가
    Fixes: #79

--- src/page/Detail/DetailContent.tsx
+++ src/page/Detail/DetailContent.tsx
@@ -3,9 +3,13 @@ import { twMerge } from 'tailwind-merge';
 
 interface DetailContentProps {
   onExpandChange: (expanded: boolean) => void; // 상태 변경 콜백
+  contentImgs: string[];
 }
 
-const DetailContent: React.FC<DetailContentProps> = ({ onExpandChange }) => {
+const DetailContent: React.FC<DetailContentProps> = ({
+  onExpandChange,
+  contentImgs,
+}) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const [showButton, setShowButton] = useState<boolean>(false);
   const imgRef = useRef<HTMLImageElement>(null);
@@ -25,32 +29,43 @@ const DetailContent: React.FC<DetailContentProps> = ({ onExpandChange }) => {
 
   useEffect(() => {
     handleImageLoad();
-  }, []);
+  }, [contentImgs]);
 
-  const ImgWrap: React.FC<{ imgsrc: string }> = ({ imgsrc }) => {
+  const ImgWrap: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const className = twMerge(
       isExpanded ? '' : 'h-[400px] overflow-hidden',
       !showButton ? 'h-auto' : ''
     );
 
     return (
-      <div className={className}>
-        <img ref={imgRef} src={imgsrc} alt='상세내용 이미지' />
+      <div className={className} ref={imgRef}>
+        {children}
       </div>
     );
   };
 
   return (
     <>
-      <ImgWrap imgsrc='https://naverbooking-phinf.pstatic.net/20210903_197/1630641195647XeHID_JPEG/210902_%BE%C6%C6%B2%B6%F5%C6%BC%BD%BA%BC%DB%B5%B5%C1%A1_%BC%D2%BC%C8_%BF%AC%B0%A3%C8%B8%BF%F8%B1%C7_%B5%F4%BB%F3%BC%BC%C6%E4%C0%CC%C1%F6_1_03.jpg?type=a1000_60_sharpen' />
-      {/* <ImgWrap imgsrc='https://image.cine21.com/resize/cine21/still/2005/1121/M0020066_focus52804[W578-].jpg' /> */}
-      {showButton && (
-        <button
-          className='w-full bg-primary text-[14px] leading-[40px] text-white'
-          onClick={handleToggleExpand}
-        >
-          {isExpanded ? '상세내용 닫기' : '상세내용 펼쳐보기'}
-        </button>
+      {contentImgs.length === 0 ? (
+        <div className='col p-[30px] text-center text-[14px] text-caption'>
+          상세내용이 없습니다.
+        </div>
+      ) : (
+        <>
+          <ImgWrap>
+            {contentImgs.map((item, i) => {
+              return <img key={i} src={item} alt={`이미지 ${i}`} />;
+            })}
+          </ImgWrap>
+          {showButton && (
+            <button
+              className='w-full bg-primary text-[14px] leading-[40px] text-white'
+              onClick={handleToggleExpand}
+            >
+              {isExpanded ? '상세내용 닫기' : '상세내용 펼쳐보기'}
+            </button>
+          )}
+        </>
       )}
     </>
   );
