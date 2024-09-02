68822537fbeb4109408e559e78446d74e0a49869
doin-N <164306935+doin-N@users.noreply.github.com>
2024-09-02 14:23:50 +0900

    ✨ Feat: 추가 서버연결 로직 추가 외3 (#97)
    * 💡 Comment: 변경 테스트용 주석처리
    
    Fixes: #9
    
    * 📚 Docs: 수정 최근본장소를 위한 타입수정
    공통 타입 export
    Fixes: #96
    
    * ✨ Feat: 추가 서버연결 로직 추가
    
    Fixes: #96
    
    * 📚 Docs: 수정 오류로 인한 타입수정
    
    Fixes: #96

--- src/page/MyPage/RecentPlace.tsx
+++ src/page/MyPage/RecentPlace.tsx
@@ -1,15 +1,50 @@
+import { useEffect } from 'react';
 import MoreTitle from '../../components/layout/MoreTitle';
 import Place from '../../components/BDPlace/Place';
 import useRecentPlacesStore from '../../store/recentPlaceStore';
 import useBookmarkStore from '../../store/bookmarkStore';
+import axios from 'axios';
+import { RegionListType } from '../../components/BDPlace/Place';
+
+interface PlaceData {
+  id: string;
+  location: string;
+  name: string;
+  rating: number;
+  reviewCount: number;
+  regionList: RegionListType[];
+}
 
 const RecentPlace: React.FC = () => {
   // Zustand 스토어에서 최근 본 장소 가져오기
-  const { recentPlaces } = useRecentPlacesStore();
+  const { recentPlaces, setRecentPlaces } = useRecentPlacesStore();
   // 북마크 스토어에서 북마크 정보 가져오기
   const { bookmarks } = useBookmarkStore();
   // 최근 본 장소를 3개로 제한하여 가져오기
   const limitedRecentPlaces = [...recentPlaces.entries()].slice(0, 3);
+
+  useEffect(() => {
+    // API 호출을 통해 최근 방문한 장소 데이터를 가져옵니다.
+    const fetchRecentPlaces = async () => {
+      try {
+        const response = await axios.get<PlaceData[]>(
+          'https://api.dogandbaby.co.kr/users/mypage/view-history/'
+        );
+        const placesData = response.data; // 데이터 구조에 따라 조정 필요
+        const placesMap = new Map<string, PlaceData>();
+
+        placesData.forEach((place) => {
+          placesMap.set(place.id, place);
+        });
+        // Zustand 상태에 최근 방문한 장소 리스트를 설정합니다.
+        setRecentPlaces(placesMap);
+      } catch (error) {
+        console.error('Failed to fetch recent places:', error);
+      }
+    };
+
+    fetchRecentPlaces();
+  }, [setRecentPlaces]);
 
   return (
     <div className='col'>
@@ -25,6 +60,7 @@ const RecentPlace: React.FC = () => {
             rating={info.rating}
             reviewCount={info.reviewCount}
             isBookmarked={!!bookmarks[placeId]} // 북마크 상태 확인
+            regionList={info.regionList}
           />
         ))}
       </div>
