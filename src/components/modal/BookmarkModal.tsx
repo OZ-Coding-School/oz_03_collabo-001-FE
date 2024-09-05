e2b647035ea89047312e1a61f4773e04636ab6ae
소빈 <sobin11@naver.com>
2024-09-05 17:15:01 +0900

    ✨ Feat: 추가 - 마이페이지 모달 커스텀스크롤 적용

--- src/components/modal/BookmarkModal.tsx
+++ src/components/modal/BookmarkModal.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { GoChevronLeft } from 'react-icons/go';
 import BookmarkList from '../../page/MyPage/Bookmark/BookmarkList';
 import useFetchCategoryData from '../../hooks/useFetchCategoryData';
+import Scrollbars from 'react-custom-scrollbars-2';
+import renderThumbVertical from '../CustomScrollbar/renderThumbVertical';
+import ScrollToTopBtn from '../CustomScrollbar/ScrollToTopBtn';
 
 interface BookMarkModalProps {
   title: string;
@@ -20,21 +23,34 @@ const BookMarkModal: React.FC<BookMarkModalProps> = ({ title, closeModal }) => {
     };
   }, []);
 
+  const scrollbarRef = useRef<Scrollbars>(null);
+
   const modalRoot = document.getElementById('modal-root');
   if (!modalRoot) return null;
   return ReactDOM.createPortal(
-    <div className='flex h-[100vh] items-start justify-center overflow-x-hidden overflow-y-scroll bg-white'>
-      <div className='w-full'>
-        <div className='col'>
-          <div className='colTitle flex h-[72px] items-center'>
-            <button onClick={closeModal} className='mr-[8px] font-extrabold'>
-              <GoChevronLeft className='text-[24px] opacity-[70%]' />
-            </button>
-            <p className='py-[18px] font-semibold'>{title}</p>
+    <div className='flex items-start justify-center overflow-x-hidden bg-white'>
+      <Scrollbars
+        style={{
+          width: '100%',
+          height: '100vh',
+        }}
+        ref={scrollbarRef}
+        renderThumbVertical={renderThumbVertical}
+        autoHide
+      >
+        <div>
+          <div className='col'>
+            <div className='colTitle flex h-[72px] items-center'>
+              <button onClick={closeModal} className='mr-[8px] font-extrabold'>
+                <GoChevronLeft className='text-[24px] opacity-[70%]' />
+              </button>
+              <p className='py-[18px] font-semibold'>{title}</p>
+            </div>
+            <BookmarkList tapRegions={tapRegions} />
           </div>
         </div>
-        <BookmarkList tapRegions={tapRegions} />
-      </div>
+        <ScrollToTopBtn scrollbarRef={scrollbarRef} />
+      </Scrollbars>
     </div>,
     document.getElementById('modal-root')!
   );
