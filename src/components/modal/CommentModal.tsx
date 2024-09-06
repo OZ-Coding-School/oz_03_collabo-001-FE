893c2a83cdae6cee6e137d698590f05341010410
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-06 14:31:47 +0900

    ✨ Feat: 추가 후기 리스트 컴포넌트 분리

--- src/components/modal/CommentModal.tsx
+++ src/components/modal/CommentModal.tsx
@@ -1,57 +1,19 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { GoChevronLeft } from 'react-icons/go';
-import axios from 'axios';
-import MyReviewListItem from '../../page/MyPage/Review/MyReviewListItem';
 import Scrollbars from 'react-custom-scrollbars-2';
 import renderThumbVertical from '../CustomScrollbar/renderThumbVertical';
 import ScrollToTopBtn from '../CustomScrollbar/ScrollToTopBtn';
+import MyReviewList from '../../page/MyPage/Review/MyReviewList';
 
 interface CommentModalProps {
   title: string;
   closeModal: () => void;
 }
 
-interface Comment {
-  id: number;
-  place_image: string;
-  place_name: string;
-  rating_point: number;
-  create_date: string;
-  content: string;
-  comments_images: string[];
-}
-
 const CommentModal: React.FC<CommentModalProps> = ({ title, closeModal }) => {
-  const [reviews, setReviews] = useState<Comment[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
   useEffect(() => {
     document.body.style.overflow = 'hidden';
-
-    const fetchReviews = async () => {
-      try {
-        const response = await axios.get<Comment[]>(
-          'http://127.0.0.1:8000/users/mypage/my-comment/',
-          { withCredentials: true }
-        );
-
-        if (response.status === 200) {
-          console.log('서버 연결 성공');
-        }
-
-        setReviews(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error('댓글 데이터를 가져오는 데 실패했습니다.', error);
-        setError('데이터를 가져오는 데 실패했습니다.');
-        setLoading(false);
-      }
-    };
-
-    fetchReviews();
-
     return () => {
       document.body.style.overflow = '';
     };
@@ -81,35 +43,7 @@ const CommentModal: React.FC<CommentModalProps> = ({ title, closeModal }) => {
               </button>
               <p className='py-[18px] font-semibold'>{title}</p>
             </div>
-            <div className='p-4'>
-              {loading && (
-                <p className='py-4 text-center text-[14px] text-caption'>
-                  로딩 중...
-                </p>
-              )}
-              {error && (
-                <p className='py-4 text-center text-[14px] text-caption'>
-                  {error}
-                </p>
-              )}
-              {reviews.length > 0 ? (
-                reviews.map((review, index) => (
-                  <MyReviewListItem
-                    key={review.id}
-                    className={index === 0 ? 'first' : ''}
-                    reviewText={review.content}
-                    placeName={review.place_name}
-                    ratingPoint={review.rating_point}
-                    createDate={review.create_date}
-                    commentImages={review.comments_images}
-                  />
-                ))
-              ) : (
-                <div className='py-4 text-center text-[14px] text-caption'>
-                  작성한 후기가 없습니다.
-                </div>
-              )}
-            </div>
+            <MyReviewList />
           </div>
         </div>
         <ScrollToTopBtn scrollbarRef={scrollbarRef} />
--- src/page/MyPage/Review/MyReviewList.tsx
+++ src/page/MyPage/Review/MyReviewList.tsx
@@ -1,11 +1,12 @@
-import MoreTitle from '../../../components/layout/MoreTitle';
-import MyReviewListItem from './MyReviewListItem';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import useInfiniteScroll from '../../../hooks/useInfiniteScroll';
+import MyReviewItem from './MyReviewItem';
+// import ScrollToTopButton from './ScrollToTopButton';
 
-interface MyReviewListProps {
-  reviews: Comment[];
-}
-
-interface Comment {
+interface ReviewData {
   id: number;
   place_image: string;
   place_name: string;
@@ -15,30 +16,106 @@ interface Comment {
   comments_images: string[];
 }
 
-const MyReviewList: React.FC<MyReviewListProps> = ({ reviews }) => {
+const fetchPlaces = async (page: number) => {
+  const params: any = {
+    page,
+    page_size: 10,
+  };
+  try {
+    const response = await axios.get(
+      'http://127.0.0.1:8000/users/mypage/view-history/',
+      { params, withCredentials: true }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('정보 가져오기 실패:', error);
+    return { results: [], next: null };
+  }
+};
+
+const MyReviewList: React.FC = () => {
+  const [reviews, setReviews] = useState<ReviewData[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const loadMorePlaces = async (initialLoad: boolean = false) => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetchPlaces(initialLoad ? 1 : page);
+
+      const newReview = response?.results || [];
+
+      if (initialLoad) {
+        if (newReview.length === 0) {
+          setError('최근 본 장소가 없습니다.');
+          setHasMore(false);
+          setReviews([]);
+        } else {
+          setReviews(newReview);
+          setPage(2);
+          setHasMore(!!response.next);
+        }
+      } else {
+        if (newReview.length === 0) {
+          setHasMore(false);
+        } else {
+          setReviews((prevPlaces) => [...prevPlaces, ...newReview]);
+          setPage((prevPage) => prevPage + 1);
+          setHasMore(!!response.next);
+        }
+      }
+    } catch (error) {
+      console.error('장소 더 가져오기 실패:', error);
+      setError('장소를 가져오는데 실패했습니다.');
+      setHasMore(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const { observerElem } = useInfiniteScroll(
+    () => loadMorePlaces(false),
+    hasMore && !isLoading
+  );
+
+  useEffect(() => {
+    loadMorePlaces(true);
+  }, []);
+
   return (
-    <>
-      <div className='col'>
-        <MoreTitle title='작성 후기' />
-        {reviews.length > 0 ? (
-          reviews.map((review, index) => (
-            <MyReviewListItem
-              key={review.id}
-              className={index === 0 ? 'first' : ''}
-              reviewText={review.content}
-              placeName={review.place_name}
-              ratingPoint={review.rating_point}
-              createDate={review.create_date}
-              commentImages={review.comments_images}
-            />
-          ))
-        ) : (
-          <div className='py-4 text-center text-[14px] text-caption'>
-            작성된 후기가 없습니다.
-          </div>
-        )}
+    <div ref={scrollContainerRef}>
+      {error && !isLoading && !reviews.length && (
+        <div className='py-4 text-center text-[14px] text-caption'>{error}</div>
+      )}
+      <div className='gap-[10px] py-2'>
+        {reviews.map((review: ReviewData, index) => (
+          <MyReviewItem
+            key={review.id}
+            className={index === 0 ? 'first' : ''}
+            reviewText={review.content}
+            placeName={review.place_name}
+            ratingPoint={review.rating_point}
+            createDate={review.create_date}
+            commentImages={review.comments_images}
+          />
+        ))}
       </div>
-    </>
+      {isLoading && (
+        <div className='py-4 text-center text-[14px] text-caption'>
+          가져오는 중...
+        </div>
+      )}
+      {hasMore && <div ref={observerElem} className='h-1' />}
+
+      {/* <ScrollToTopButton scrollContainerRef={scrollContainerRef} /> */}
+    </div>
   );
 };
 
